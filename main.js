d7ac554cf11ae1a53480e9b537edbac304ed60af
João Victor da Silva <jv147002@gmail.com>
2023-04-03 16:58:13 -0300

    terminando o java script da ultima tela

--- main.js
+++ main.js
@@ -1,50 +1,92 @@
 'use strict'
 
-import {button} from "./module/api.js"
+// Tela 1 finalizada
 
-const botaoCurso = await button()
+// import {button} from "./module/api.js"
 
-
-const criandoCardCursos = (curso, indice) => {
-
-    const divButtons = document.createElement('div');
-    divButtons.classList.add('container-buttons');
-
-    const divCurso = document.createElement('div');
-    divCurso.classList.add('cards-tag');
-
-    const cardCurso = document.createElement('div');
-    cardCurso.classList.add(`curso_${curso.sigla}`);
-
-    const imgCurso = document.createElement('img');
-    imgCurso.classList.add(`img-${curso.sigla}`);
-    imgCurso.src = curso.icone
-
-    const textCurso = document.createElement('p');
-    textCurso.classList.add(`${curso.sigla}`);
-    textCurso.textContent = curso.sigla;
-
-    cardCurso.append(imgCurso, textCurso);
+// const botaoCurso = await button()
 
 
-    divButtons.append(cardCurso);
+// const criandoCardCursos = (curso, indice) => {
+
+//     const divButtons = document.createElement('div');
+//     divButtons.classList.add('container-buttons');
+
+//     const divCurso = document.createElement('div');
+//     divCurso.classList.add('cards-tag');
+
+//     const cardCurso = document.createElement('div');
+//     cardCurso.classList.add(`curso_${curso.sigla}`);
+
+//     const imgCurso = document.createElement('img');
+//     imgCurso.classList.add(`img-${curso.sigla}`);
+//     imgCurso.src = curso.icone
+
+//     const textCurso = document.createElement('p');
+//     textCurso.classList.add(`${curso.sigla}`);
+//     textCurso.textContent = curso.sigla;
+
+//     cardCurso.append(imgCurso, textCurso);
 
 
-    divButtons.onclick = () => {
-        carregarCurso(indice)
+//     divButtons.append(cardCurso);
+
+
+//     divButtons.onclick = () => {
+//         carregarCurso(indice)
+//     }
+//     return divButtons;
+
+
+
+// }
+
+// const carregarCurso = () => {
+//     const cardPrincipal = document.getElementById('card-principal')
+//     const componentesCards = botaoCurso.cursos.map(criandoCardCursos)
+
+//     cardPrincipal.replaceChildren(...componentesCards)
+// }
+
+// carregarCurso()
+
+// tela 2 finalizando 
+
+import { alunos } from "./module/script2.js";
+
+
+const criandoCarAluno = (aluno, indice ) => {
+
+    const divAluno = document.createElement('div')
+    divAluno.classList.add('cardAluno')
+
+    const divFoto = document.createElement('div')
+    divAluno.classList.add('fotoAluno')
+
+    const img = document.createElement('img')
+    img.classList.add('img-aluno')
+    img.src = aluno.foto
+
+    divFoto.append(img)
+
+    divAluno.append(divFoto)
+
+    divAluno.onclick = () => {
+        carregarAluno_grafico(indice)
     }
-    return divButtons;
 
-
+    return divAluno
 
 }
 
-const carregarCurso = () => {
-    const cardPrincipal = document.getElementById('card-principal')
-    const componentesCards = botaoCurso.cursos.map(criandoCardCursos)
+const carregarAluno_grafico = () => {
+    console.log(alunos);
+    const cardAluno = document.getElementById('cardAluno')
+    const componentesCards = alunos.map(criandoCarAluno)
 
-    cardPrincipal.replaceChildren(...componentesCards)
+    cardAluno.replaceChildren(...componentesCards)
 }
 
-carregarCurso()
+
+carregarAluno_grafico()
 
