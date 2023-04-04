52eb5648b0e56dd717392833470b3dfc041780dc
Dan Lino <danielalinodasilvaoliveira@gmail.com>
2023-04-04 01:16:11 -0300

    começando as funções js

--- module/api.js
+++ module/api.js
@@ -8,3 +8,19 @@ export const button = async () =>{
 
     return data
 }
+
+const getAlunos = async (filtro) =>{
+
+    const urlTodosAlunos = `https://dull-rose-quail-yoke.cyclic.app/v1/lion-school/alunos`;
+    const responseTodosAlunos = await fetch(urlTodosAlunos);
+    const dataTodosAlunos = await responseTodosAlunos.json();
+
+    const urlAlunosCurso = `https://dull-rose-quail-yoke.cyclic.app/v1/lion-school/alunos?cursos=${filtro}`
+    const responseAlunosCurso = await fetch(urlAlunosCurso);
+    const dataAlunosCurso = await responseAlunosCurso.json();
+
+    const urlAlunosStatus = `https://dull-rose-quail-yoke.cyclic.app/v1/lion-school/alunos?status=${filtro}`
+    const responseAlunosStatus = await fetch(urlAlunosStatus);
+    const dataAlunosStatus = await responseAlunosStatus.json();
+
+}
